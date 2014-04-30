ef49388280662fb36af4d92916c4f313b8318198
rosyang <yang.xy9@gmail.com>
2014-04-30 00:22:02 -0400

    Create QuickCrab.java

---
+++ QuickCrab.java
@@ -0,0 +1,56 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.util.ArrayList;
+
+/**
+ * A <code>QuickCrab</code> looks at a limited set of neighbors when it eats and moves.
+ * <br />
+ */
+public class QuickCrab extends CrabCritter
+{
+
+    /**
+     * @return list of empty locations immediately two spaces to the right and to the left
+     */
+    public ArrayList<Location> getMoveLocations()
+    {
+        ArrayList<Location> locs = new ArrayList<Location>();
+        int[] dirs =
+            { Location.LEFT, Location.RIGHT };
+        for (Location loc : getLocationsInDirections(dirs))
+            if (getGrid().get(loc) == null)
+                locs.add(loc);
+
+        return locs;
+    }
+
+    /**
+     * Finds the valid adjacent locations of this critter in different
+     * directions.
+     * @param directions - an array of directions (which are relative to the
+     * current direction)
+     * @return a set of valid locations that are neighbors of the current
+     * location in the given directions
+     */
+    public ArrayList<Location> getLocationsInDirections(int[] directions)
+    {
+        ArrayList<Location> locs = new ArrayList<Location>();
+        Grid gr = getGrid();
+        Location loc = getLocation();
+    
+        for (int d : directions)
+        {
+            Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+            Location neighborLoc2 = neighborLoc.getAdjacentLocation(getDirection());
+            if (gr.isValid(neighborLoc))
+                locs.add(neighborLoc);
+	    if (gr.isValid(neighborLoc2))
+		locs.add(neighborLoc2);
+        }
+        return locs;
+    }  
+
+}
