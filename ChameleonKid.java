31f6d7dfe0241d0361ce38b7343d17309fbd0b00
rosyang <yang.xy9@gmail.com>
2014-05-02 00:40:55 -0400

    Update ChameleonKid.java
    Tried different approach after looking at QuickCrab

--- ChameleonKid.java
+++ ChameleonKid.java
@@ -1,6 +1,7 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.actor.Critter;
 import info.gridworld.grid.Location;
+import info.gridworld.grid.Grid;
 
 import java.util.ArrayList;
 
@@ -20,13 +21,34 @@ public class ChameleonKid extends ChameleonCritter
      */
     public ArrayList<Actor> getActors()
     {
-	Location front = getLocation().getAdjacentLocation( getDirection() ); //actor immediately in front
-	Location back = getLocation().getAdjacentLocation( getDirection() + 180 ); //actor immediately behind
-
-	ArrayList<Actor> arr = new ArrayList<Actor>();
-	arr.add( getGrid().get( front ) );
-	arr.add( getGrid().get( back ) );
+        ArrayList<Actor> arr = new ArrayList<Actor>();
+        int[] dirs =
+            { Location.AHEAD, Location.HALF_CIRCLE };
+        for (Location loc : getLocationsInDirections(dirs))
+            if (getGrid().get(loc) != null)
+                arr.add(getGrid().get(loc));
         return arr;
+        
+     /**
+     * Finds the valid adjacent locations of this critter in different
+     * directions.
+     * @param directions - an array of directions (which are relative to the
+     * current direction)
+     * @return a set of valid locations that are neighbors of the current
+     * location in the given directions
+     */
+    public ArrayList<Location> getLocationsInDirections(int[] directions)
+    {
+        ArrayList<Location> locs = new ArrayList<Location>();
+        Grid gr = getGrid();
+        Location loc = getLocation();
+    
+        for (int d : directions)
+        {
+            Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+            if (gr.isValid(neighborLoc))
+                locs.add(neighborLoc);
+        }
+        return locs;
     }
-
 }
